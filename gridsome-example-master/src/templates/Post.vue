46880995d3ec994c07b27e17561c8d08c841fad0
Esteban <esteban.salazar.p@gmail.com>
2019-07-12 12:22:18 -0400

    first commit blog adolescentes

---
+++ gridsome-example-master/src/templates/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <BlogLayout>
+    <div class="markdown-body">
+      <h1>{{ $page.post.title }}</h1>
+      <img :src="$page.post.featuredImage" alt="blog">
+      <div v-html="$page.post.content" />
+    </div>
+  </BlogLayout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+  post: post (path: $path) {
+    title
+    content
+    featuredImage
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    }
+  }
+}
+</script>
